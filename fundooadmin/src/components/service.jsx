fcb29a40cfcf06ed0578ebc145ef569552d217c2
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-06-22 20:56:38 +0530

    [GowriK] Refactor : console log is taken,removed the unwanted lines and changed the component name to camelcase

--- fundooadmin/src/components/service.jsx
+++ fundooadmin/src/components/service.jsx
@@ -13,7 +13,7 @@ import Modal from "@material-ui/core/Modal";
 import Snackbar from "@material-ui/core/Snackbar";
 import { IconButton } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
-import FullWidthTabs from "./Tabs";
+import FullWidthTabs from "./tabs";
 
 const drawerWidth = 240;
 
