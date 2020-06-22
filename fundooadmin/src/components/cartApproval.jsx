dc2c3a3e01031409bc147385adbd76747775dcdb
GowriRajeshwari <gowrikanaga216@gmail.com>
2020-06-22 21:05:00 +0530

    [GowriK] Refactor : refactor is done

---
+++ fundooadmin/src/components/cartApproval.jsx
@@ -0,0 +1,203 @@
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import profile from "../assets/profile.png";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import {
+  userCartList,
+  adminCompleteOrder,
+  adminCancelOrder,
+} from "../services/LoginService";
+import Snackbar from "@material-ui/core/Snackbar";
+import { IconButton } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ClipLoader from "react-spinners/ClipLoader";
+
+function searchigFor(query) {
+  return function (search) {
+    return (
+      search.user.firstName.toLowerCase().includes(query.toLowerCase()) ||
+      search.product.name.toLowerCase().includes(query.toLowerCase()) ||
+      !query
+    );
+  };
+}
+class CartApproval extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      query: this.props.query,
+      snackbaropen: false,
+      address: [],
+      loading: true,
+    };
+  }
+  componentDidMount = () => {
+    userCartList().then((response) => {
+      if (response.status === 200) {
+        this.setState({ data: response.data.data });
+        for (let i = 0; i < response.data.data.length; i++) {
+          if (response.data.data[i].user.addresses != undefined) {
+            this.state.address.push(
+              response.data.data[i].user.addresses[0].address
+            );
+          } else {
+            this.state.address.push("undefined");
+          }
+        }
+        this.setState({ address: this.state.address, loading: false });
+      } else {
+        this.setState({
+          snackbarmsg:
+            "Login Not Successfull,Make sure email & password is correct",
+          snackbaropen: true,
+        });
+      }
+    });
+  };
+  message = (message) => {
+    var content = message.replace(/<[^>]*>/g, "");
+    return content;
+  };
+  acceptQuestion = (id) => {
+    let data = {
+      cartId: id,
+    };
+    adminCompleteOrder(data).then((response) => {
+      if (response.status === 200) {
+        this.componentDidMount();
+        this.setState({
+          snackbarmsg: "OrderPlaced is accepted",
+          snackbaropen: true,
+        });
+      } else {
+        this.setState({
+          snackbarmsg: "Order not accepted",
+          snackbaropen: true,
+        });
+      }
+    });
+  };
+  rejectQuestion = (id) => {
+    let data = {
+      cartId: id,
+    };
+    adminCancelOrder(data).then((response) => {
+      if (response.status === 200) {
+        this.componentDidMount();
+        this.setState({ snackbarmsg: "Order is rejected", snackbaropen: true });
+      } else {
+        this.setState({
+          snackbarmsg: "Order not rejected",
+          snackbaropen: true,
+        });
+      }
+    });
+  };
+  handleClose = (event) => {
+    this.setState({ snackbaropen: false });
+  };
+  close = () => {};
+  render() {
+    return (
+      <div className="firstcontainer">
+        <div className="detailcontainer2">
+          <div className="rowWise">
+            <div>
+              <div className="stylefont1">Cart Details</div>
+            </div>
+            <div className="widthApproval">
+              <div className="stylefont1">Approval</div>
+            </div>
+          </div>
+          <div>
+            <ClipLoader
+              css={{ width: "50px", height: "50px", marginLeft: "50%" }}
+              size={150}
+              color={"#123abc"}
+              loading={this.state.loading}
+            />
+          </div>
+          {this.state.data
+            .filter(searchigFor(this.props.query))
+            .map((data, index) => (
+              <div>
+                <Divider />
+                <div key={index} className="row">
+                  <div className="questionPart">
+                    <div className="stylefont">
+                      {data.user.firstName} {data.user.lastName}
+                    </div>
+                    <div>
+                      <div className="stylefont">
+                        service : {data.product.name} Price : $
+                        {data.product.price}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="stylefont">
+                        address : {this.state.address[index]}
+                      </div>
+                    </div>
+                    <div>
+                      <div className="stylefont"></div>
+                    </div>
+                  </div>
+
+                  <div className="alignItemCenter">
+                    {data.isApproved ? (
+                      <div className="approved">
+                        <div>Accept</div>
+                      </div>
+                    ) : (
+                      <div
+                        className="approved1"
+                        onClick={() => this.acceptQuestion(data.id)}
+                      >
+                        <div style={{ padding: "2px" }}>Accept</div>
+                      </div>
+                    )}
+                    {data.isCanceled ? (
+                      <div className="canceled">
+                        <div>Reject</div>
+                      </div>
+                    ) : (
+                      <div
+                        className="canceled1"
+                        onClick={() => this.rejectQuestion(data.id)}
+                      >
+                        <div style={{ padding: "2px" }}>Reject</div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <Divider />
+              </div>
+            ))}
+        </div>
+        <Snackbar
+          open={this.state.snackbaropen}
+          autoHideDuration={4000}
+          onClose={this.handleClose}
+          message={<span>{this.state.snackbarmsg}</span>}
+          action={[
+            <IconButton
+              key="close"
+              arial-label="close"
+              color="inherit"
+              onClick={this.handleClose}
+            >
+              x
+            </IconButton>,
+          ]}
+        ></Snackbar>
+      </div>
+    );
+  }
+}
+export default CartApproval;
---
+++ fundooadmin/src/components/resetPassword.jsx
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import profile from "../assets/profile.png";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import { resetPassword } from "../services/LoginService";
+import Snackbar from "@material-ui/core/Snackbar";
+import { IconButton } from "@material-ui/core";
+
+class ResetPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helperText: "",
+      error: false,
+      username: "",
+      userdata: [],
+      show: false,
+      login: "Reset Password",
+      next: false,
+      password: "",
+      helperTextpassowrd: "",
+      confirmPassword: "",
+      helperTextCpassowrd: "",
+      snackbaropen: false,
+      snackbarmsg: "",
+      confirmpassword: "",
+      pass: null,
+    };
+  }
+  Reset = (event) => {
+    event.preventDefault();
+    this.validator();
+    let data = {
+      newPassword: this.state.password,
+    };
+
+    const id = localStorage.getItem("id");
+
+    if (
+      this.state.helperTextpassowrd === "" &&
+      this.state.helperTextCpassowrd === ""
+    ) {
+      if (this.state.pass == true) {
+        resetPassword(data, id).then((response) => {
+          if (response.statusText == "No Content") {
+            this.setState({
+              snackbaropen: true,
+              snackbarmsg: "Succefully changed.",
+            });
+
+            this.props.history.push({
+              pathname: "/",
+            });
+          } else {
+            this.setState({
+              snackbarmsg: "Password not successfull",
+              snackbaropen: true,
+            });
+          }
+        });
+      }
+    } else {
+      this.setState({
+        snackbarmsg: "Make sure password and confirm is correct",
+        snackbaropen: true,
+      });
+    }
+  };
+  validator = () => {
+    if (this.state.password != "") {
+      if (
+        /[\@\#\$\%\^\&\*\(\)\_\+\!]/.test(this.state.password) &&
+        /[a-z]/.test(this.state.password) &&
+        /[0-9]/.test(this.state.password) &&
+        /[A-Z]/.test(this.state.password)
+      ) {
+        this.setState({
+          password: this.state.password,
+          helperTextpassowrd: "",
+          error: false,
+        });
+      } else {
+        this.setState({
+          helperTextpassowrd: "Min 8 char, at least 1 letter,1 no & 1 spl char",
+          error: true,
+          password: this.state.password,
+        });
+      }
+    } else if (this.state.password == "") {
+      this.setState({
+        helperTextpassowrd: "Enter the password",
+        error: true,
+        password: this.state.password,
+      });
+    }
+    if (this.state.confirmpassword == "") {
+      this.setState({
+        helperTextCpassowrd: "Enter the confirm password",
+        error: true,
+        confirmpassword: this.state.confirmpassword,
+      });
+    } else {
+      this.checkPassword();
+    }
+  };
+
+  //close snackbar
+  handleClose = (event) => {
+    this.setState({ snackbaropen: false });
+  };
+  onchangePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  onchangePasswordagain = (event) => {
+    this.setState({
+      confirmpassword: event.target.value,
+    });
+  };
+
+  checkPassword() {
+    if (this.state.password === this.state.confirmpassword) {
+      this.setState({
+        confirmpassword: this.state.confirmpassword,
+        helperTextCpassowrd: "",
+        error: false,
+      });
+    } else {
+      this.setState({
+        confirmpassword: this.state.confirmpassword,
+        helperTextCpassowrd: "Password should be equal",
+        error: false,
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="firstcontainerReset">
+        <span class="usernameReset">
+          <span>F</span>
+          <span>u</span>
+          <span>n</span>
+          <span>D</span>
+          <span>o</span>
+          <span>o</span>
+        </span>
+        <div className="loginstyleReset">{this.state.login}</div>
+
+        <Paper id="rootpaperReset">
+          <div className="containerReset">
+            <div className="borderReset">
+              <div className="loginFromReset">
+                <div className="inputFieldReset">
+                  <TextField
+                    hintText="Password"
+                    floatingLabelText="Password"
+                    id="btnReset"
+                    variant="outlined"
+                    type="password"
+                    label="NewPassword"
+                    helperText={this.state.helperTextpassowrd}
+                    error={this.state.helperTextpassowrd}
+                    onChange={this.onchangePassword}
+                  />
+                </div>
+                <div className="inputFieldReset">
+                  <TextField
+                    hintText="Password"
+                    floatingLabelText="Password"
+                    id="btnReset"
+                    variant="outlined"
+                    type="password"
+                    label="Re-enter New Password"
+                    helperText={this.state.helperTextCpassowrd}
+                    error={this.state.helperTextCpassowrd}
+                    onChange={this.onchangePasswordagain}
+                  />
+                </div>
+
+                <div className="submitButtonReset">
+                  <Button id="subbtnReset" onClick={(e) => this.Reset(e)}>
+                    Change Password
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Paper>
+        <Snackbar
+          open={this.state.snackbaropen}
+          autoHideDuration={6000}
+          onClose={this.handleClose}
+          message={<span>{this.state.snackbarmsg}</span>}
+          action={[
+            <IconButton
+              key="close"
+              arial-label="close"
+              color="inherit"
+              onClick={this.handleClose}
+            >
+              x
+            </IconButton>,
+          ]}
+        ></Snackbar>
+      </div>
+    );
+  }
+}
+export default ResetPassword;
